a6723be397d48c5f655aecfce5b1127aa04ed90a
Alexey Mogilyovkin <zeroicq@gmail.com>
2018-04-26 11:04:15 +0000

    Refactor

--- src/actors.cpp
+++ src/actors.cpp
@@ -48,43 +48,6 @@ void game::MainCharActor::move(game::GameControls controls, shared_ptr<game::Map
     map->get_cell(desired_row, desired_col)->actor()->collide(*this, map);
 }
 
-
-
-//void game::GuardActor::move(game::GameControls controls, game::Map& map) {
-//    ActiveActor::move(controls, map);
-//    enum Directions {up, right, down, left, count};
-//
-//    std::random_device rd;
-//    std::mt19937 gen(rd());
-//    auto gen_rnd_direction = uniform_int_distribution<>(0, Directions::count-1);
-//
-//    int desired_row = row();
-//    int desired_col = col();
-//
-//    switch (gen_rnd_direction(gen)) {
-//        case Directions::up:
-//            desired_row -= 1;
-//            break;
-//        case Directions::right:
-//            desired_col += 1;
-//            break;
-//        case Directions::down:
-//            desired_row += 1;
-//            break;
-//        case Directions::left:
-//            desired_col -= 1;
-//            break;
-//        default:
-//            break;
-//    }
-//
-//    if (!map.is_inbound(desired_row, desired_col)) {
-//        return;
-//    }
-//
-//    map.get_cell(desired_row, desired_col)->actor()->collide(*this, map);
-//}
-
 shared_ptr<game::MapCell> game::Actor::get_shared_ptr(int row, int col, shared_ptr<game::Map> map)
 {
     return map->get_cell(row, col);
